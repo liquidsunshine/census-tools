d73ec7a88e5b2190ad68cc45d939ecec4a18720f
Donald J. Hayward <liquidsunshine@gmail.com>
2013-01-31 23:29:10 -0500

    UTF-8/Unicode support fixed.

--- csvize_geoheaders/geoheader.py
+++ csvize_geoheaders/geoheader.py
@@ -29,7 +29,7 @@ compile_csv = True
 
 # -- Stuff -- #
 
-import ftputil, ftplib, os, zipfile, csv
+import ftputil, ftplib, os, zipfile, csv, codecs
 # TODO: Find a better way of handling the file structure... config files perhaps?
 structure2010 = (("FILEID",6,0,"VARCHAR"), ("STUSAB",2,6,"VARCHAR"), ("SUMLEV",3,8,"VARCHAR"), ("GEOCOMP",2,11,"VARCHAR"), ("CHARITER",3,13,"VARCHAR"), ("CIFSN",2,16,"VARCHAR"), ("LOGRECNO",7,18,"NUMERIC"), ("REGION",1,25,"VARCHAR"), ("DIVISION",1,26,"VARCHAR"), ("STATE",2,27,"VARCHAR"), ("COUNTY",3,29,"VARCHAR"), ("COUNTYCC",2,32,"VARCHAR"), ("COUNTYSC",2,34,"VARCHAR"), ("COUSUB",5,36,"VARCHAR"), ("COUSUBCC",2,41,"VARCHAR"), ("COUSUBSC",2,43,"VARCHAR"), ("PLACE",5,45,"VARCHAR"), ("PLACECC",2,50,"VARCHAR"), ("PLACESC",2,52,"VARCHAR"), ("TRACT",6,54,"VARCHAR"), ("BLKGRP",1,60,"VARCHAR"), ("BLOCK",4,61,"VARCHAR"), ("IUC",2,65,"VARCHAR"), ("CONCIT",5,67,"VARCHAR"), ("CONCITCC",2,72,"VARCHAR"), ("CONCITSC",2,74,"VARCHAR"), ("AIANHH",4,76,"VARCHAR"), ("AIANHHFP",5,80,"VARCHAR"), ("AIANHHCC",2,85,"VARCHAR"), ("AIHHTLI",1,87,"VARCHAR"), ("AITSCE",3,88,"VARCHAR"), ("AITS",5,91,"VARCHAR"), ("AITSCC",2,96,"VARCHAR"), ("TTRACT",6,98,"VARCHAR"), ("TBLKGRP",1,104,"VARCHAR"), ("ANRC",5,105,"VARCHAR"), 
 ("ANRCCC",2,110,"VARCHAR"), ("CBSA",5,112,"VARCHAR"), ("CBSASC",2,117,"VARCHAR"), ("METDIV",5,119,"VARCHAR"), ("CSA",3,124,"VARCHAR"), ("NECTA",5,127,"VARCHAR"), ("NECTASC",2,132,"VARCHAR"), ("NECTADIV",5,134,"VARCHAR"), ("CNECTA",3,139,"VARCHAR"), ("CBSAPCI",1,142,"VARCHAR"), ("NECTAPCI",1,143,"VARCHAR"), ("UA",5,144,"VARCHAR"), ("UASC",2,149,"VARCHAR"), ("UATYPE",1,151,"VARCHAR"), ("UR",1,152,"VARCHAR"), ("CD",2,153,"VARCHAR"), ("SLDU",3,155,"VARCHAR"), ("SLDL",3,158,"VARCHAR"), ("VTD",6,161,"VARCHAR"), ("VTDI",1,167,"VARCHAR"), ("RESERVE2",3,168,"VARCHAR"), ("ZCTA5",5,171,"VARCHAR"), ("SUBMCD",5,176,"VARCHAR"), ("SUBMCDCC",2,181,"VARCHAR"), ("SDELM",5,183,"VARCHAR"), ("SDSEC",5,188,"VARCHAR"), ("SDUNI",5,193,"VARCHAR"), ("AREALAND",14,198,"NUMERIC"), ("AREAWATR",14,212,"NUMERIC"), ("NAME",90,226,"VARCHAR"), ("FUNCSTAT",1,316,"VARCHAR"), ("GCUNI",1,317,"VARCHAR"), ("POP100",9,318,"NUMERIC"), ("HU100",9,327,"NUMERIC"), ("INTPTLAT",11,336,"VARCHAR"), ("INTPTLON",12,347,"VARCHAR"), ("LSADC",2,359,"VARCHAR"), 
@@ -53,9 +53,9 @@ def parse_record(rec_str, structure = structure2010, return_type = "dict"):
   return record
   
 def parse_file(filename, structure = structure2010, return_type = "dict"):
-  with open(filename, 'r') as f:
+  with codecs.open(filename, 'r', encoding="latin1") as f:
     for rec_str in f:
-      yield parse_record( rec_str.decode('latin-1').encode('utf8'), structure, return_type )
+      yield parse_record( unicode(rec_str), structure, return_type )
       
 def get_sql(structure = structure2010):
   return "CREATE TABLE geoheader ({0});".format( ', '.join( "{0} {1}({2})".format(col_id, var_type, length) for (col_id, length, start, var_type) in structure ) )
@@ -85,6 +85,7 @@ if __name__ == '__main__':
 		print "Extracting {0}".format(zf.filename)
 		state_zip.extract(zf, data_folder)
 
+  # Compile everything into one CSV
   if compile_csv:
     with open(output_folder+'all_geoheaders.csv', 'wb') as csvfile:
       writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -94,9 +95,10 @@ if __name__ == '__main__':
 	  record_list = parse_file(data_folder+f, return_type="list")
 	  print "Writing {0} to CSV".format(f)
 	  for r in record_list:
-	    writer.writerow(r)  
+	    writer.writerow([col.encode("utf-8") for col in r])
 	  del record_list
 
+  # Generate SQL
   if generate_sql:
     with open(output_folder+'create_table.sql', 'wb') as sqlfile:
       sqlfile.write( get_sql() )
